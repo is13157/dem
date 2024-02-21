ee95dcc54cbaea2d0766185d0f2ab6837b3c1565
student <student@a>
2024-02-21 08:41:52 +0700

    update

--- www/app/Models/Perfomance.php
+++ www/app/Models/Perfomance.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Perfomance extends Model
 {
     use HasFactory;
+
+    public function genre():HasMany
+    {
+        return $this->hasMany(Genre::class);
+    }
+
+    public function age():HasOne
+    {
+        return $this->hasOne(Age::class);
+    }
 }
